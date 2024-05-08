9255b71dca4feabe81b18b7e77285a7aa53ff250
Luke <luke.zhao@yilupay.com>
2024-05-08 14:33:01 +0800

    提交

---
+++ src/main/java/org/luke/multi/MultifunctionalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.luke.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultifunctionalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultifunctionalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/luke/multi/MultifunctionalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.luke.multi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultifunctionalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
